e7e2ea15f5e7be343006960d3dd0c7b88cd9512d
aartzz <migor1103@gmail.com>
2026-08-01 11:52:46 +0300

    fix(messages): fix transparent messages losing transparency on animations

--- src/main/java/ni/shikatu/re_extera/hooks/chatmessagecell/TransparentDeletedMessages.java
+++ src/main/java/ni/shikatu/re_extera/hooks/chatmessagecell/TransparentDeletedMessages.java
@@ -17,34 +17,9 @@ public class TransparentDeletedMessages extends XC_MethodHook {
             View cell = (View) param.thisObject;
             
             if (messageObject != null) {
-                boolean isDeleted = false;
-                try {
-                    if (cell instanceof org.telegram.ui.Cells.ChatMessageCell) {
-                        org.telegram.ui.Cells.ChatMessageCell chatCell = (org.telegram.ui.Cells.ChatMessageCell) cell;
-                        org.telegram.messenger.MessageObject.GroupedMessages group = chatCell.getCurrentMessagesGroup();
-                        if (group != null && group.messages != null && !group.messages.isEmpty()) {
-                            isDeleted = true;
-                            for (MessageObject m : group.messages) {
-                                if (m != null && !m.deleted && !ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(m)) {
-                                    isDeleted = false;
-                                    break;
-                                }
-                            }
-                        } else {
-                            isDeleted = messageObject.deleted || ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(messageObject);
-                        }
-                    } else {
-                        isDeleted = messageObject.deleted || ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(messageObject);
-                    }
-                } catch (Throwable e) {
-                    isDeleted = messageObject.deleted || ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(messageObject);
-                }
-                
-                if (isDeleted && !messageObject.deletedByThanos) {
-                    cell.setAlpha(Settings.getTransparentDeletedMessagesAlpha());
-                } else {
-                    cell.setAlpha(1.0f);
-                }
+                // We just call setAlpha(1.0f). The TransparentDeletedMessagesAlpha hook 
+                // will intercept this call and apply transparency if the message is deleted.
+                cell.setAlpha(1.0f);
             }
         } catch (Throwable e) {
             android.util.Log.e("re-extera", "TransparentDeletedMessages error", e);
--- src/main/java/ni/shikatu/re_extera/hooks/chatmessagecell/TransparentDeletedMessagesAlpha.java
+++ src/main/java/ni/shikatu/re_extera/hooks/chatmessagecell/TransparentDeletedMessagesAlpha.java
@@ -6,6 +6,8 @@ import ni.shikatu.re_extera.settings.Settings;
 import java.lang.reflect.Method;
 
 public class TransparentDeletedMessagesAlpha extends XC_MethodHook {
+    private static Method getMessageObjectMethod;
+
     @Override
     public void beforeHookedMethod(MethodHookParam param) {
         if (!ni.shikatu.re_extera.hooks.HookInit.isActive || !Settings.getTransparentDeletedMessages()) {
@@ -13,11 +15,35 @@ public class TransparentDeletedMessagesAlpha extends XC_MethodHook {
         }
         
         try {
-            Method getMessageObjectMethod = param.thisObject.getClass().getMethod("getMessageObject");
+            if (getMessageObjectMethod == null) {
+                getMessageObjectMethod = param.thisObject.getClass().getMethod("getMessageObject");
+            }
             MessageObject messageObject = (MessageObject) getMessageObjectMethod.invoke(param.thisObject);
             
             if (messageObject != null) {
-                boolean isDeleted = messageObject.deleted || ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(messageObject);
+                boolean isDeleted = false;
+                try {
+                    if (param.thisObject instanceof org.telegram.ui.Cells.ChatMessageCell) {
+                        org.telegram.ui.Cells.ChatMessageCell chatCell = (org.telegram.ui.Cells.ChatMessageCell) param.thisObject;
+                        org.telegram.messenger.MessageObject.GroupedMessages group = chatCell.getCurrentMessagesGroup();
+                        if (group != null && group.messages != null && !group.messages.isEmpty()) {
+                            isDeleted = true;
+                            for (MessageObject m : group.messages) {
+                                if (m != null && !m.deleted && !ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(m)) {
+                                    isDeleted = false;
+                                    break;
+                                }
+                            }
+                        } else {
+                            isDeleted = messageObject.deleted || ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(messageObject);
+                        }
+                    } else {
+                        isDeleted = messageObject.deleted || ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(messageObject);
+                    }
+                } catch (Throwable e) {
+                    isDeleted = messageObject.deleted || ni.shikatu.re_extera.db.ReExteraDb.get().messageIsDeleted(messageObject);
+                }
+
                 if (isDeleted && !messageObject.deletedByThanos) {
                     float currentAlpha = (float) param.args[0];
                     param.args[0] = currentAlpha * Settings.getTransparentDeletedMessagesAlpha();
