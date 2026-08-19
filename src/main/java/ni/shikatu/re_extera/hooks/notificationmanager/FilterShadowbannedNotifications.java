1aa310ecb9966d781ad8e3b2af1aa802e7070b53
aartzz <migor1103@gmail.com>
2026-08-19 13:40:28 +0300

    fix(online-status): record last online timestamp from incoming messages in notifications pipeline

--- src/main/java/ni/shikatu/re_extera/hooks/notificationmanager/FilterShadowbannedNotifications.java
+++ src/main/java/ni/shikatu/re_extera/hooks/notificationmanager/FilterShadowbannedNotifications.java
@@ -4,6 +4,8 @@ import de.robv.android.xposed.XC_MethodHook;
 import java.util.ArrayList;
 import java.util.Iterator;
 import ni.shikatu.re_extera.Main;
+import ni.shikatu.re_extera.db.ReExteraDb;
+import ni.shikatu.re_extera.settings.Settings;
 import ni.shikatu.re_extera.utils.ShadowbanCache;
 import org.telegram.messenger.MessageObject;
 
@@ -13,12 +15,16 @@ public class FilterShadowbannedNotifications extends XC_MethodHook {
         if (messages == null || messages.isEmpty()) {
             return;
         }
+        boolean saveLastOnline = Settings.getSaveLastOnline();
         Iterator<MessageObject> iterator = messages.iterator();
         while (iterator.hasNext()) {
             MessageObject message = iterator.next();
             if (message != null && !message.isOut()) {
                 long fromId = message.getFromChatId();
                 if (fromId > 0) {
+                    if (saveLastOnline && message.messageOwner != null && message.messageOwner.date > 0) {
+                        ReExteraDb.get().saveLastOnlineAsync(fromId, message.messageOwner.date);
+                    }
                     boolean isDm = message.getDialogId() > 0;
                     if (ShadowbanCache.shouldHideInGroups(fromId) || (isDm && ShadowbanCache.shouldHideDialog(fromId))) {
                         Main.log("FilterShadowbannedNotifications: filtered from %d (dm=%b)", Long.valueOf(fromId), Boolean.valueOf(isDm));
