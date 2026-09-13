b1f036761c28e62cfc96642e80e29b711e86304c
Smashh <ru.sak.ilya2011@gmail.com>
2026-09-13 14:34:32 +0300

    Add files via upload

--- src/main/java/ni/shikatu/re_extera/hooks/navigation/DrawerMenuGhostHook.java
+++ src/main/java/ni/shikatu/re_extera/hooks/navigation/DrawerMenuGhostHook.java
@@ -15,5 +15,20 @@ public class DrawerMenuGhostHook extends XC_MethodHook {
         int currentAccount = ((Integer) param.args[0]).intValue();
         BaseFragment fragment = (BaseFragment) param.args[1];
         GhostMenuHelper.injectIntoDrawer(drawerMenuView, currentAccount, fragment);
+        // Вставляем пункт "Настройки" сразу после призрака. Т.к. GhostMenuHelper
+        // мог не вставить свой пункт (если призрак скрыт настройкой), используем
+        // текущее количество children контейнера как безопасный ориентир конца списка.
+        ni.shikatu.re_extera.utils.SettingsMenuHelper.injectIntoDrawer(drawerMenuView, currentAccount, getContainerChildCount(drawerMenuView));
+    }
+
+    private static int getContainerChildCount(DrawerMenuView drawerMenuView) {
+        try {
+            java.lang.reflect.Field f = DrawerMenuView.class.getDeclaredField("container");
+            f.setAccessible(true);
+            android.widget.LinearLayout container = (android.widget.LinearLayout) f.get(drawerMenuView);
+            return container != null ? container.getChildCount() : 0;
+        } catch (Throwable e) {
+            return 0;
+        }
     }
 }
